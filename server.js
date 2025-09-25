9fd8a3057063804f8c3571729c7f1f9a3c752956
Sarthak Gupta <sarthakgup619@gmail.com>
2025-09-25 21:01:14 +0530

    initialize server with express, cors, dotenv, and passport configuration

--- server.js
+++ server.js
@@ -64,7 +64,8 @@ connectCloudinary();
 //middleware
 app.use(express.json());
 app.use(cors({ 
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  // origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL ,
   credentials: true
 }));
 
