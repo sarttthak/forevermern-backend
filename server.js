04fb9f51df17fb11fc82192b1d08af3e2af08a95
Sarthak Gupta <sarthakgup619@gmail.com>
2025-09-25 21:56:32 +0530

    refactor CORS middleware to support specific origins and methods

--- server.js
+++ server.js
@@ -64,10 +64,27 @@ connectCloudinary();
 //middleware
 app.use(express.json());
 app.use(cors({ 
-  // origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   origin: process.env.FRONTEND_URL ,
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+
+const allowedOrigins = [
+  "http://localhost:5173",   // dev frontend
+  "https://forevermern-frontend.onrender.com" // prod frontend
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 
 // Initialize Passport
 app.use(passport.initialize());
